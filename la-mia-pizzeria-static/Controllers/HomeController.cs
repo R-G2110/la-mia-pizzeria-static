3bde7d8d25ce8683458eb385d68e5151794be034
R-G2110 <argeldelacruz86@gmail.com>
2024-05-10 17:05:34 +0200

    [Add]: pizza salsiccia&friarielli

--- la-mia-pizzeria-static/Controllers/HomeController.cs
+++ la-mia-pizzeria-static/Controllers/HomeController.cs
@@ -23,7 +23,8 @@ namespace la_mia_pizzeria_static.Controllers
                 new Pizza("Pizza alla Marinara", "Semplice e deliziosa, con pomodoro, aglio, origano e olio extravergine di oliva.", "~/img/marinara.jpg", 4.99m),
                 new Pizza("Pizza alla Diavola", "Una pizza piccante con salame piccante, peperoncino e mozzarella.", "~/img/diavola.jpg", 6.49m),
                 new Pizza("Pizza con Mortadella, Stracchino e Pesto", "Una combinazione di mortadella, stracchino e pesto", "~/img/mortadella&stracchino.jpg", 7.99m),
-                new Pizza("Pizza Capricciosa", "Una pizza classica con prosciutto cotto, funghi, carciofi, olive e mozzarella.", "~/img/capricciosa.jpg", 8.99m)
+                new Pizza("Pizza Capricciosa", "Una pizza classica con prosciutto cotto, funghi, carciofi, olive e mozzarella.", "~/img/capricciosa.jpg", 8.99m),
+                new Pizza("Salsiccia e Friarielli", "Una pizza tipica della tradizione napoletana, con salsiccia e friarielli, condita con mozzarella di bufala e pomodoro San Marzano.", "~/img/salsiccia&friarielli.jpg", 8.99m)
             };
 
             return View(pizze); 
